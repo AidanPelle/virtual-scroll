83c6d97805050d50e988bcae559d281cee33a76b
Aidan Pelle <aidan.pelle@workhub.com>
2025-03-18 18:23:55 -0600

    Set up library

---
+++ projects/virtual-scroll/src/lib/virtual-scroll.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'virtual-scroll',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      virtual-scroll works!
+    </p>
+  `,
+  styles: ``
+})
+export class VirtualScrollComponent {
+
+}
---
+++ projects/virtual-scroll/src/public-api.ts
@@ -0,0 +1,6 @@
+/*
+ * Public API Surface of virtual-scroll
+ */
+
+export * from './lib/virtual-scroll.service';
+export * from './lib/virtual-scroll.component';
