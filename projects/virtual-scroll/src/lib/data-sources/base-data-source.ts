aa201daf123b5d52f9a0ab22515d04d790b9d4d0
Aidan Pelle <aidan.pelle@workhub.com>
2025-03-18 20:10:15 -0600

    Add necessary property to abstract dataSource

--- projects/virtual-scroll/src/lib/data-sources/base-data-source.ts
+++ projects/virtual-scroll/src/lib/data-sources/base-data-source.ts
@@ -1,5 +1,6 @@
 import { DataSource } from "@angular/cdk/collections";
+import { BehaviorSubject } from "rxjs";
 
 export abstract class BaseDataSource<T> extends DataSource<T> {
-    
+    abstract dataListener: BehaviorSubject<T[]>;
 }
