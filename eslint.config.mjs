507b02b94e855d27855f60bb3297726c8991f0f9
Aidan Pelle <aidan.pelle@workhub.com>
2025-05-02 12:19:58 -0600

    Re-add json with comments

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,7 @@ export default defineConfig([
   { files: ["**/*.{js,mjs,cjs,ts}"], languageOptions: { globals: { ...globals.browser, ...globals.node } } },
   tseslint.configs.recommended,
   { files: ["**/*.json"], plugins: { json }, language: "json/json", extends: ["json/recommended"] },
+  { files: ["**/*.jsonc"], plugins: { json }, language: "json/jsonc", extends: ["json/recommended"] },
   { files: ["**/*.md"], plugins: { markdown }, language: "markdown/gfm", extends: ["markdown/recommended"] },
   {
     rules: {
