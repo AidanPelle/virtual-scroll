3e869e75d6c7e7b8123f4db68bb727ec90e1c6a7
Aidan Pelle <aidan.pelle@workhub.com>
2025-05-15 16:51:01 -0600

    added more testing

--- projects/virtual-scroll/src/lib/column-manager/column-manager.spec.ts
+++ projects/virtual-scroll/src/lib/column-manager/column-manager.spec.ts
@@ -1,15 +1,16 @@
 import { Component, EmbeddedViewRef, TemplateRef, ViewChild, ViewContainerRef } from '@angular/core';
 import { VirtualScrollComponent } from '../virtual-scroll/virtual-scroll.component';
 import { ColumnManager } from './column-manager';
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { VirtualScrollModule } from '../virtual-scroll.module';
-import { BehaviorSubject, delay, firstValueFrom, of } from 'rxjs';
+import { BehaviorSubject, firstValueFrom } from 'rxjs';
 import { CellContext } from '../interfaces/cell-context';
-import { UtilityService } from '../utility.service';
+import { By } from '@angular/platform-browser';
 
 describe('VsRowComponent', () => {
   let manager: ColumnManager<unknown>;
   let virtualScroll: VirtualScrollComponent<unknown>;
+  let fixture: ComponentFixture<ColumnManagerTest>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -18,9 +19,9 @@ describe('VsRowComponent', () => {
     })
     .compileComponents();
     
-    const virtualScrollFixture = TestBed.createComponent(ColumnManagerTest);
-    const instance = virtualScrollFixture.componentInstance;
-    virtualScrollFixture.detectChanges();
+    fixture = TestBed.createComponent(ColumnManagerTest);
+    const instance = fixture.componentInstance;
+    fixture.detectChanges();
 
     manager = new ColumnManager(
         instance.viewContainer,
@@ -40,6 +41,31 @@ describe('VsRowComponent', () => {
     expect(manager).toBeTruthy();
   });
 
+  it('should remove widths', () => {
+    const renderedElement = manager.renderedCellViews.find(c => c.columnName === 'Min Width Cell')?.view.rootNodes[0] as HTMLElement;
+    expect(renderedElement.style.minWidth).toEqual('50px');
+    virtualScroll.removeCellWidths.next('Min Width Cell');
+    expect(renderedElement.style.minWidth).toEqual('');
+  });
+
+  it('should remove the rendered cell', async () => {
+    let divs = fixture.debugElement.queryAll(By.css('div'));
+    let cell2Element = divs.find(d => (d.nativeElement as HTMLElement).textContent?.includes('Cell 2'));
+    expect(cell2Element).toBeTruthy();
+
+    const orderedCellDefs = await firstValueFrom(virtualScroll.orderedCellDefs$);
+    const cellDef2 = orderedCellDefs.find(o => o.columnName === 'Cell 2');
+    cellDef2?.userDefinedActiveState.next(false);
+
+    divs = fixture.debugElement.queryAll(By.css('div'));
+    cell2Element = divs.find(d => (d.nativeElement as HTMLElement).textContent?.includes('Cell 2'));
+    expect(cell2Element).toBeFalsy();
+  });
+
+  it('should move the cell', async () => {
+    // virtualScroll = 
+    // let cell2Element = divs.find(d => (d.nativeElement as HTMLElement).textContent?.includes('Cell 2'));
+  });
 });
 
 @Component({
@@ -48,10 +74,14 @@ describe('VsRowComponent', () => {
       <div *cellDef="let item; name: 'Cell 1';">Cell 1</div>
       <div *cellDef="let item; name: 'Cell 2';">Cell 2</div>
       <div *cellDef="let item; name: 'Cell 3'; isActive: false;">Cell 3</div>
-      <div *cellDef="let item; name: 'Min Width Cell'; minWidth: 50;">Cell 3</div>
+      <div *cellDef="let item; name: 'Min Width Cell'; minWidth: 50;">Min Width Cell</div>
     </virtual-scroll>
 
     <ng-container #viewContainer></ng-container>
+    
+    <ng-template #sliderTemplate>
+      <div style="min-width: 8px; max-width: 8px; height: 100%; box-sizing: border-box;"></div>
+    </ng-template>
   `,
 })
 class ColumnManagerTest {
