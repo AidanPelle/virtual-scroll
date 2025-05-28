5e3d2127e6f8a60b2f61f59dbd4d393186b6a2c7
Aidan Pelle <aidan.b.pelle@gmail.com>
2025-05-27 22:24:23 -0600

    Added more comments and formatting

--- projects/virtual-scroll/src/lib/outlets/row-outlet.directive.ts
+++ projects/virtual-scroll/src/lib/outlets/row-outlet.directive.ts
@@ -13,46 +13,65 @@ import { BaseDataSource } from "../data-sources/base-data-source";
 export class RowOutletDirective<T> implements OnInit, OnDestroy {
 
   private _viewContainer = inject(ViewContainerRef);
+
+  /** The helper class that handles rendering and destroying columns within a row, to separate out logic. */
   public _columnManager?: ColumnManager<T>;
 
+  /** A custom template defined by the user for how they want to display a row in the table. */
   @Input() rowTemplate?: TemplateRef<unknown>;
 
+  /** A fallback row template for when none has been defined by the user. */
   @Input() defaultRowTemplate!: TemplateRef<unknown>;
 
+  /**
+   * A template defined internally for displaying the loading animation on rows while asynchronously waiting for data,
+   * or whipping the scrollbar.
+   */
   @Input() loadingRowTemplate!: TemplateRef<unknown>;
 
+  /** A template defined internally for the slider between the columns, for resizing columns. */
   @Input() sliderTemplate!: TemplateRef<unknown>;
 
+  /** A reference to the parent component, using typeof to handle circular references. */
   @Input() virtualScroll!: typeof VirtualScrollComponent.prototype;
 
+  /** A reference to the related object for this given row in the overarching list. */
   @Input() item!: T;
 
+  /** The index at which this row exists in the list. */
   @Input() index!: number;
 
+  /** The source object currently in use, used to find if we need to skip the loading animation for a row. */
   @Input() dataSource?: BaseDataSource<T> | null;
 
-  private _onDestroy = new Subject<void>();
+  /** A reference to the currently rendered row template, used for adding css classes. */
+  public rowView?: EmbeddedViewRef<unknown>;
 
+  /** BehaviorSubject that emits whenever the column manager renders a sticky column. */
   protected renderSticky = new BehaviorSubject<EmbeddedViewRef<CellContext<T>> | null>(null);
-  public renderedSticky$ = this.renderSticky.pipe(takeUntil(this._onDestroy));
+  
+  /** Subject to handle cleaning up ongoing observables when this row no longer exists. */
+  private _onDestroy = new Subject<void>();
+  
+  /** A reference to the renderSticky BehaviorSubject, scoped for when this row is destroyed. */
+  public renderSticky$ = this.renderSticky.pipe(takeUntil(this._onDestroy));
 
   ngOnInit(): void {
     if (this.dataSource?.skipLoadAnimation(this.index)) {
-      this.renderRow();
+      this._renderRow();
       return;
     }
 
-    this.renderPlaceholderRow();
+    this._renderPlaceholderRow();
     of(0).pipe(
       delay(RENDER_DELAY),
       takeUntil(this._onDestroy),
       filter(() => this.item !== undefined)
-    ).subscribe(() => this.renderRow());
-  }
+    ).subscribe(() => this._renderRow());
+  }  
 
-  public rowView?: EmbeddedViewRef<unknown>;
-
-  renderPlaceholderRow() {
+  /** Renders the default loading row animation while we wait for the real data. */
+  _renderPlaceholderRow() {
     // Remove any currently rendered items from the view
     this._viewContainer.clear();
 
@@ -60,7 +79,8 @@ export class RowOutletDirective<T> implements OnInit, OnDestroy {
     this.rowView.rootNodes[0].children[0].style.animationDelay = '-' + ((this.index % 10) / 2) + 's';
   }
 
-  renderRow(): void {
+  /** Renders the row template (custom if provided, default otherwise) and hands off to the column manager for individual columns. */
+  _renderRow(): void {
     // Remove any currently rendered items from the view
     this._viewContainer.clear();
 
@@ -82,6 +102,7 @@ export class RowOutletDirective<T> implements OnInit, OnDestroy {
     this.initColumnManager(cellOutlet);
   }
 
+  /** Just instantiates this row's column manager. */
   protected initColumnManager(cellOutlet: CellOutletDirective): void {
     this._columnManager = new ColumnManager(
       cellOutlet.viewContainer,
